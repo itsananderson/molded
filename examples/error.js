1218bd752d3738328e08e7a3e321caa08cfa2e5d
Will Anderson <will@itsananderson.com>
2014-07-13 04:33:55 -0700

    Update error example file with next() usage

--- examples/error.js
+++ examples/error.js
@@ -5,6 +5,10 @@ app.get('/', function() {
     throw Error('Something went wrong');
 });
 
+app.get('/next', function(next) {
+    next(Error('Something went wrong next'));
+});
+
 app.error(function(res, err)  {
     res.statusCode = err.status || 500;
     res.send({message: err.message, error: err});
