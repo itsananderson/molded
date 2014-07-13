3aa71addb476f8a6dc2d3d9c7fa36ac2a13c0549
Will Anderson <will@itsananderson.com>
2014-07-12 21:45:27 -0700

    Update examples with more routes and middleware

--- examples/simple.js
+++ examples/simple.js
@@ -1,13 +1,16 @@
 var injector = require('../');
 var serveStatic = require('serve-static');
 var serveIndex = require('serve-index');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 var app = injector();
 
-app.value('port', 8080);
+app.value('port', 3000);
 
+app.use(logger('dev'));
 app.use(serveStatic(__dirname));
-app.use(serveIndex(__dirname));
+app.use(bodyParser.json());
 
 app.get('/greet/:name/:age', function(req, res) {
     res.send('hello ' + req.params.name +
@@ -17,5 +20,15 @@ app.get('/greet/:name/:age', function(req, res) {
 app.get(/^\/foo\/bar(.*)$/, function(req, res) {
     res.send(req.params[0]);
 });
+
+app.get('/json', function(req, res) {
+    res.send({here:'is',some:'json'});
+});
+
+app.post('/json', function(req, res) {
+    res.send(req.body);
+});
+
+app.use(serveIndex(__dirname));
 
 app.listen(app.value('port'));
