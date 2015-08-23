22ffee4b95e6bd1070b21d2f2976b20018777e6e
Will Anderson <will@itsananderson.com>
2015-08-23 13:49:07 -0700

    Update 'injection' example to use molded.inject

--- examples/injection.js
+++ examples/injection.js
@@ -22,17 +22,17 @@ app.use('/:user', molded.provide('user', function(params, users) {
     return users[params.user];
 }));
 
-app.get('/me', function(sendJson, user1) {
-    sendJson(user1);
-});
+app.get('/me', molded.inject(function(res, user1) {
+    res.send(user1);
+}));
 
-app.get('/:user', function(sendJson, next, params, user) {
+app.get('/:user', molded.inject(function(req, res, user) {
     if (user) {
         sendJson(user);
     } else {
         sendJson({error: params.user + ' does not exist'});
     }
-});
+}));
 
 /* istanbul ignore else */
 if (module.parent) {
